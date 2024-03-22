ddc71db52c26728b2a39f9d211f921a6996aa0fd
Herve Peroteau <herve.peroteau@icloud.com>
2024-03-22 21:17:38 +0100

    Setup

---
+++ TCA_Tutorial/CounterFeature.swift
@@ -0,0 +1,8 @@
+//
+//  CounterFeature.swift
+//  TCA_Tutorial
+//
+//  Created by Herve Peroteau on 22/03/2024.
+//
+
+import ComposableArchitecture
---
+++ TCA_Tutorial/TCA_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_TutorialApp.swift
+//  TCA_Tutorial
+//
+//  Created by Herve Peroteau on 22/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TCA_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
