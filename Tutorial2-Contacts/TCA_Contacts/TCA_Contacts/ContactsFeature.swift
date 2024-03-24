4592e8216a0f37d75699504de1965d05aeb7c22d
Herve Peroteau <herve.peroteau@icloud.com>
2024-03-24 16:35:19 +0100

    Tutorial 2 : multiple destination from a feature

--- Tutorial2-Contacts/TCA_Contacts/TCA_Contacts/ContactsFeature.swift
+++ Tutorial2-Contacts/TCA_Contacts/TCA_Contacts/ContactsFeature.swift
@@ -13,38 +13,66 @@ struct Contact: Equatable, Identifiable {
     var name: String
 }
 
+extension ContactsFeature {
+    @Reducer(state: .equatable)
+    enum Destination {
+        case addContact(AddContactFeature)
+        case alert(AlertState<ContactsFeature.Action.Alert>)
+    }
+}
+
 @Reducer
 struct ContactsFeature {
     @ObservableState
     struct State: Equatable {
         var contacts: IdentifiedArrayOf<Contact> = []
-        @Presents var addContact: AddContactFeature.State?
-    }
-    
-    enum Action {
-        case addButtonTapped
-        case addContact(PresentationAction<AddContactFeature.Action>)
+        @Presents var destination: Destination.State?
     }
 
+    enum Action {
+        case addButtonTapped
+        case deleteButtonTapped(id: Contact.ID)
+        case destination(PresentationAction<Destination.Action>)
+        enum Alert: Equatable {
+            case confirmDeletion(id: Contact.ID)
+        }
+    }
+    
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
                 case .addButtonTapped:
-                    state.addContact = AddContactFeature.State(
-                        contact: Contact(id: UUID(), name: "")
+                    state.destination = .addContact(
+                        AddContactFeature.State(
+                            contact: Contact(id: UUID(), name: "")
+                        )
                     )
                     return .none
 
-                case let .addContact(.presented(.delegate(.saveContact(contact)))):
+                case let .destination(.presented(.addContact(.delegate(.saveContact(contact))))):
                     state.contacts.append(contact)
                     return .none
 
-                case .addContact:
+                case let .destination(.presented(.alert(.confirmDeletion(id: id)))):
+                    state.contacts.remove(id: id)
+                    return .none
+
+                case .destination:
+                    return .none
+
+                case let .deleteButtonTapped(id: id):
+                    state.destination = .alert(
+                        AlertState {
+                            TextState("Are you sure?")
+                        } actions: {
+                            ButtonState(role: .destructive, action: .confirmDeletion(id: id)) {
+                                TextState("Delete")
+                            }
+                        }
+                    )
                     return .none
             }
         }
-        .ifLet(\.$addContact, action: \.addContact) {
-            AddContactFeature()
-        }
+        .ifLet(\.$destination, action: \.destination)
     }
 }
